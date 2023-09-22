d1cf1a081229a9abc0c532022b8e4ef801a4c1dc
Jason_song97 <jaegeunsong97@gmail.com>
2023-09-23 00:16:39 +0900

    fix : Card에서  이미지 가져오기

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,7 @@
 <template>
      <div class="card shadow-sm">
-          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          <img :src="item.imgPath" />
+
           <div class="card-body">
           <p class="card-text">{{ item.name }}</p>
           <div class="d-flex justify-content-between align-items-center">
