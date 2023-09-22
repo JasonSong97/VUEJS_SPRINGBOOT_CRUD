4a19bbef80c80261f04a5fc4bb7671b1ebe56550
Jason_song97 <jaegeunsong97@gmail.com>
2023-09-23 00:39:27 +0900

    fix : 할인가, 할인가에 , 붙이기

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,8 +5,16 @@
           <p class="card-text">{{ item.name }}</p>
           <div class="d-flex justify-content-between align-items-center">
                <button class="btn btn-primary">구입하기</button> 
-               <small class="text-body-secondary">
-                    {{ lib.getNumberFormatted(item.price) }} 원
+               <small class="price text-muted">
+                    {{ lib.getNumberFormatted(item.price) }}원
+               </small>
+
+               <small class="discount text-danger">
+                    {{ item.discountPer }}%
+               </small>
+
+               <small class="real text-danger">
+                    {{ lib.getNumberFormatted(item.price - (item.price * item.discountPer / 100)) }}원
                </small>
           </div>
           </div>
@@ -35,5 +43,9 @@ export default {
      height:250px;
      background-size: cover;
      background-position: center;
+}
+
+.card .card-body .price{
+     text-decoration: line-through;
 }
 </style>
