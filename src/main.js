efda51cab1c4512e5acfb0f248d44990d627c843
Jason_song97 <jaegeunsong97@gmail.com>
2023-09-23 08:25:58 +0900

    setting : 라우터 설치

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from "@/pages/Home";
 
-createApp(App).mount('#app')
+const routes = [
+     // / 경로로 오면 Home으로 보냄
+     {path:'/', component: Home}
+]
+
+const router = createRouter({
+     history: createWebHistory(), 
+     routes
+})
+
+createApp(App).use(router).mount('#app') // 라우터를 사용함
+ 
