158cd54bf89622901b6248d3f01a191915cf217f
Jason_song97 <jaegeunsong97@gmail.com>
2023-09-23 14:46:35 +0900

    fix : v-model로 반응형 선언하기

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,16 +2,14 @@
   <div class="form-signin w-100 m-auto">
   <form>
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="state.form.email">
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="state.form.password">
       <label for="floatingPassword">Password</label>
     </div>
-
     <div class="form-check text-start my-3">
       <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
       <label class="form-check-label" for="flexCheckDefault">
@@ -26,8 +24,20 @@
 
 
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+  setup(){
+    // 반응형 선언
+    const state = reactive({
+      form: {
+        email: "",
+        password: ""
+      }
+    })
+
+    return {state}
+  }
 }
 </script>
 
