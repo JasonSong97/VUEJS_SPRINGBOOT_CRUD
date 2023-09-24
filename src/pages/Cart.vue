59784dda1934a3442fa138e4fb3219c9057d0930
Jason_song97 <jaegeunsong97@gmail.com>
2023-09-24 14:21:12 +0900

    장바구니 화면에 데이터 받아와서 렌더링

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="cart">
-    장바구니 페이지
+    <div class="container">
+      <ul>
+        <li v-for="(item, idx) in state.items" :key="idx">
+          <img :src="item.imgPath"/>
+          <span class="name">{{ item.name }}</span>
+          <span class="name">{{ item.price }}</span>
+        </li>
+      </ul>
+    </div>>
   </div>
 </template>
 
 <script>
+import { reactive } from "vue";
 import axios from "axios";
 
 export default {
   setup(){
+    const state = reactive({
+      items:[]
+    })
+
     axios.get("/api/cart/items").then(({data}) => {
         console.log(data);
+        state.items = data;
     })
+
+    return {state}
   }
 }
 </script>
